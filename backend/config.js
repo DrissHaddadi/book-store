8306681d31380c0f42df7498318d183a3b7b7558
DrissHaddadi <driss.haddadi@outlook.fr>
2023-09-09 18:56:28 +0200

    DB created and CRUD operations done for books

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const PORT = 5555;
+
+export const MONGODB_URL = process.env.MONGODB_URL;
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,29 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, MONGODB_URL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoutes from "./routes/booksRoutes.js";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port : ${PORT}`);
+// Middleware for parsing request body
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to my Book Store");
 });
+
+app.use("/books", booksRoutes);
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log("App connected to the database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port : ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
